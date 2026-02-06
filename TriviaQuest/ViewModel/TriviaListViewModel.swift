c024e9ad264dbe63f9d82c6efb5e12f190403adf
Edgar Agno <edgardo76@yahoo.com>
2026-02-06 11:29:14 +0000

    Failing tests with implementation to be implemented in order to pass.

--- TriviaQuest/ViewModel/TriviaListViewModel.swift
+++ TriviaQuest/ViewModel/TriviaListViewModel.swift
@@ -1,33 +1,43 @@
 import Observation
+import Foundation
 
 @Observable
 class TriviaListViewModel {
     
-    var trivias: [TriviaItemViewModel]
     var isLoading: Bool
     var refreshError: Error?
+    var searchText: String?
+    var difficultyFilter: TriviaDifficulty?
+    var categoryFilter: TriviaCategory?
     
     let navigationTitle = "Trivia Quest"
     let api: TriviaAPIProviding
     
     init(api: TriviaAPIProviding) {
         self.api = api
-        self.trivias = []
         self.isLoading = false
         self.refreshError = nil
+        self._trivias = []
     }
+    
+    var trivias: [TriviaItemViewModel] {
+        []
+    }
+    
+    private var _trivias: [TriviaItemViewModel]
     
     func fetch() async {
         do {
             isLoading = true
             let trivias = try await api.fetch()
-            self.trivias = trivias.map { .init(trivia: $0)  }
+            _trivias = trivias.map { .init(trivia: $0)  }
             isLoading = false
             refreshError = nil
         } catch {
             self.refreshError = error
+            _trivias = []
             isLoading = false
         }
     }
-    
+        
 }
--- TriviaQuestTests/TriviaListViewModelTests.swift
+++ TriviaQuestTests/TriviaListViewModelTests.swift
@@ -22,7 +22,7 @@ struct TriviaListViewModelTests {
         #expect(sut.trivias.last?.category == .videoGames)
         #expect(sut.trivias.count == 15)
     }
-    
+        
     @Test("view model emits refresh error")
     func testRefreshErrorEmitted() async {
         let data = Data("invalid data".utf8)
@@ -34,5 +34,46 @@ struct TriviaListViewModelTests {
         
         #expect(sut.isLoading == false)
         #expect(sut.refreshError != nil)
+        #expect(sut.trivias.count == 0)
     }
+    
+    @Test("test view model search text filter")
+    func testSearchTextFilter() async {
+        let data = StubData.sampleJSON.data(using: .utf8)!
+        let session = getMockSession(from: data)
+        let api = await TriviaAPI(session: session)
+        let sut = TriviaListViewModel(api: api)
+        await sut.fetch()
+        
+        sut.searchText = "how"
+                
+        #expect(sut.trivias.count == 1)
+    }
+    
+    @Test("test view model difficulty filter")
+    func testDifficultyFilter() async {
+        let data = StubData.sampleJSON.data(using: .utf8)!
+        let session = getMockSession(from: data)
+        let api = await TriviaAPI(session: session)
+        let sut = TriviaListViewModel(api: api)
+        await sut.fetch()
+        
+        sut.difficultyFilter = .medium
+     
+        #expect(sut.trivias.count == 11)
+    }
+    
+    @Test("test view model with category filter")
+    func testCategoryFilter() async {
+        let data = StubData.sampleJSON.data(using: .utf8)!
+        let session = getMockSession(from: data)
+        let api = await TriviaAPI(session: session)
+        let sut = TriviaListViewModel(api: api)
+        await sut.fetch()
+        
+        sut.categoryFilter = .music
+        
+        #expect(sut.trivias.count == 2)
+    }
+    
 }
